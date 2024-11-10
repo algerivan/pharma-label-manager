55fc643f6436c68bbbd115ecbc77d13ffeab7e45
algerivan <algerzm@gmail.com>
2024-11-09 18:44:56 -0700

    add antd

---
+++ src/app/_utils/colors.utils.ts
@@ -0,0 +1,41 @@
+export const COLORS = {
+  primary: '#007BFF',
+  secondary: '#ADB5BD',
+  tertiary: '#B48BFF',
+  text: '#343A40',
+  'secondary-text': '#6C757D',
+  background: '#F3F3F3',
+  'colored-background': '#00AEFF',
+  card: {
+    background: '#FCFCFC',
+    border: '#E0E0E0',
+  },
+  button: {
+    background: '#007BFF',
+    text: '#FFFFFF',
+    'outline-border': '#ABABAB',
+  },
+  alert: {
+    background: '#F66D9B',
+    text: '#FFFFFF',
+  },
+  input: {
+    border: '#ADB5BD',
+    background: '#FFFFFF',
+    text: '#343A40',
+  },
+  feedback: {
+    success: '#28A745',
+    warning: '#FFC107',
+    error: '#DC3545',
+  },
+  chip: {
+    background: '#B8DAFF',
+    text: '#007BFF',
+  },
+  divider: '#E0E0E0',
+  select: {
+    selected: '#D8D8D8',
+    background: '#FFFFFF',
+  },
+};
